65f96afa9ada34d6c1ec23ff06dcbfae72104da3
VadymSorokin <s.a.rock.in619@gmail.com>
2021-03-27 01:40:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const calculator = require('./calculator');
+	
+calculator.set(10);
+console.log(calculator.add(2));
+console.log(calculator.sub(5));
+console.log(calculator.mult(4));
+console.log(calculator.div(2));
---
+++ calculator.js
@@ -0,0 +1,13 @@
+const set = require('./core/setNumber');
+const add = require('./core/add');
+const sub = require('./core/substract');
+const div = require('./core/divide');
+const mult = require('./core/multiply');
+
+module.exports = {
+	set,
+	add,
+	sub,
+	div,
+	mult,
+}
